f8634df33c1565bde7c5669ef96fa65ce520fa01
jamshidjon199 <130271430+jamshidjon199@users.noreply.github.com>
2023-04-09 14:38:22 +0500

    Don-Don

---
+++ main.js
@@ -0,0 +1,9 @@
+const choices = document.querySelector('.choice'),
+score = document.querySelector('#score'),
+modal = document.querySelector('.modal'),
+result = document.querySelector('#result'),
+restart = document.querySelector('#restart')
+scoreBoard = {
+    player: 0,
+    computer: 0
+};
